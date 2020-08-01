7c9db3dc5ec4d521637563ce012ac26bedece1de
luzcry <mcrodriguezt618@gmail.com>
2020-07-31 21:14:42 -0500

    1st commit

---
+++ app.js
@@ -0,0 +1,35 @@
+new Vue ({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function() {
+            let max = 10;
+            let min = 3;
+            let damage= Math.max(Math.floor(Math.random() * max) + 1, min)
+            this.monsterHealth -= damage;
+
+            max = 12;
+            min= 5;
+            damage= Math.max(Math.floor(Math.random() * max) + 1, min)
+            this.playerHealth -= damage;
+        }, 
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
+        }
+    }
+});
